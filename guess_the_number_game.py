cad46e6582608f7da7c43c1786d86cdc93812a44
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2020-09-01 14:54:01 +0200

    Change language to english

--- guess_the_number_game.py
+++ guess_the_number_game.py
@@ -3,12 +3,12 @@ digit = randint(1,100)
 decision = False
 n = 0
 while decision == False:
-    guess = int(input('Zgadnij jaką liczbę wylosował komputer: '))
+    guess = int(input('Guess which number computer chose: '))
     n += 1
     if guess < digit:
-        print("Twoja liczba jest WIĘKSZA od tej, którą wylosował komputer")
+        print("Your number is LOWER than computer's choosen number")
     elif guess > digit:
-        print('Twoja liczba jest MNIEJSZA od tej, którą wylosował komputer')
+        print("Your number is GREATER than computer's chosen number")
     else:
-        print('Brawo! Udało Ci się zgadnąć liczbę! Udało Ci się to zrobić w {} próbach!'.format(n))
+        print('Congrats! You guessed the number correctly! You did it in {} tries!'.format(n))
         decision = True
