836b751977b3abcaa9e48a61182d9d107fa84755
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2020-09-02 17:17:20 +0200

    Add time measure

--- Sorting_algorithms.py
+++ Sorting_algorithms.py
@@ -7,7 +7,23 @@ lst = [[random.randint(1,100) for i in range(10)],
        [random.randint(1,100) for i in range(100)],
        [random.randint(1,100) for i in range(1000)],
        [random.randint(1,100) for i in range(10000)]]
+
+#----------------- Time measure -------------#
+def timeit(method):
+    def timed(lst):
+        measurement = {'Elements':[],'Time':[]}
+        for i in lst:
+            start = time.time()
+            method(i)
+            end = time.time()
+            time_elapsed = end - start
+            measurement['Elements'].append(len(i))
+            measurement['Time'].append(time_elapsed)
+        return (tabulate(measurement,headers=measurement.keys()))
+    return timed
+
 #--------------- Bubble sort ---------------#
+@timeit
 def bubble_sort(lst):
     for j in range(len(lst) - 1):
         for i in range(len(lst) - 1):
@@ -15,16 +31,31 @@ def bubble_sort(lst):
                 lst[i],lst[i+1] = lst[i+1],lst[i]
     return lst
 
-print('Bubble sort:')
-measurment = {'Elements':[],'Time':[]}
-for i in lst:
-    start = time.time()
-    bubble_sort(i)
-    end = time.time()
-    time_elapsed = end - start
-    measurment['Elements'].append(len(i))
-    measurment['Time'].append(time_elapsed)
-print(tabulate(measurment,headers=measurment.keys()))
-print('\n')
 
+print('Bubble sort:')
+print(bubble_sort(lst))
+print('\n\n')
 #------------------- Quick sort -------------------#
+
+def quick_sort(lst):
+    if len(lst) < 2:
+        return lst
+    else:
+        lower = []
+        greater = []
+        equal = []
+        pivot = lst[0]
+        for i in lst:
+            if i < pivot:
+                lower.append(i)
+            elif i > pivot:
+                greater.append(i)
+            elif i == pivot:
+                equal.append(i)
+        return quick_sort(lower) + equal + quick_sort(greater)
+        
+
+
+print('Quick sort:')
+print(timeit(quick_sort)(lst))
+print('\n\n')
