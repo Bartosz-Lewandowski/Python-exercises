d8382b6aa2561515067eb58e16f033965384e18c
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2020-09-02 12:52:08 +0200

    Clean up code

--- simple_algorithms.py
+++ simple_algorithms.py
@@ -1,41 +1,33 @@
+#--------------------1-----------------------------------#
+#Find if given word is a polindrom
 def polindrom(word):
     word = word.lower()
     word = word.replace(' ','')
     reversed_word = word[::-1]
     if reversed_word == word:
-        return('That word is a polindrom!')
+        return('Word {} is a polindrom!'.format(word))
     else:
-        return('That word is not a polindrom.')
-#print(polindrom('kajak'))
-#print(polindrom('Agata'))
-#print(polindrom('Ala'))
-#print(polindrom('a to kanapa pana kota'))
+        return('Word {} is not a polindrom.'.format(word))
+print(polindrom('bbbaaaabbb'))
+print(polindrom('coronavirus'))
+print(polindrom('aea'))
+print(polindrom('a to kanapa pana kota'))
+print('\n\n')
 
+#------------------2------------------#
+#Find common letters in two given words
 def common_letters(word1,word2):
     common = list(set(word1)&set(word2))
     for i in common:
         yield i
+print('Common letters:')
+for i in common_letters('Hello!','Hi! How are you?'):
+    print(i)
+    
+print('\n')
 
-#for i in common_letters('Hello!','Hi! How are you?'):
-#    print(i)
-
-def bubble_sort(a):
-    for j in range(len(a) - 1):
-        for i in range(len(a) - 1):
-            if a[i] > a[i+1]:
-                a[i],a[i+1] = a[i+1],a[i]
-    return a
-#print(bubble_sort([3,4,2,90,112,4,3,1,0]))
-
-def star_tringle(n):
-    for i in range(1,n + 1):
-        if i % 2 != 0:
-            ran = n - i
-            yield int((ran/2)) * ' ' + i * '*' + int((ran/2)) * ' ' 
-
-for i in star_tringle(7):
-  print(i)
-
+#--------------------4------------------#
+#Fibonacci algorithm
 def fibonacci(n):
     result = []
     for i in range(1,n+1):
@@ -45,17 +37,20 @@ def fibonacci(n):
             new_digit = result[i-2] + result[i-3]
             result.append(new_digit)
     return result
+print('Fibonacci with list:\n)
+print(fibonacci(5)
+print('\n')
 
-# print(fibonacci(3))
-
+#Fibonacci with recurency
 def fib_rek(n):
     if n < 1:
         return 0
     if n < 2:
         return 1
     return fib_rek(n - 1) + fib_rek(n - 2)
-
-#print(fib_rek(10))
+print('Fibonacci with recurency:\n')
+print(fib_rek(10))
+print('\n\n')
 
 import random 
 def estimate_pi(n):
