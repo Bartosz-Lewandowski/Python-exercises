aed2cd4b0aa164a17f862fe92ada4b1b1f2c0a43
Bartosz-Lewandowski <112126@student.upwr.edu.pl>
2020-09-08 14:51:32 +0200

    Add comments

--- simple_tasks/Readme.md
+++ simple_tasks/Readme.md
@@ -1,5 +1,8 @@
-# Simple tasks
-## This is readme
-* Task 1
-* Task 2
-* Task 3
+# Simple tasks to practice code
+## In file guess_the_number_game
+There is simple and well known game to guess the number which computer randomly chose. After guess program tells user if his guess was too high or maybe lower than searching number. 
+## In file simple_tasks
+* Task 1 - Program tells if given word is polindrom 
+* Task 2 - Program finds common letters in two given words
+* Task 3 - Fibonacci algorithm using lists and using recursion
+* Task 4 - Estimating pi value
--- simple_tasks/simple_algorithms.py
+++ simple_tasks/simple_algorithms.py
@@ -8,6 +8,7 @@ def polindrom(word):
         return('Word {} is a polindrom!'.format(word))
     else:
         return('Word {} is not a polindrom.'.format(word))
+print('-------------------Task 1-----------------------')
 print(polindrom('bbbaaaabbb'))
 print(polindrom('coronavirus'))
 print(polindrom('aea'))
@@ -20,13 +21,15 @@ def common_letters(word1,word2):
     common = list(set(word1)&set(word2))
     for i in common:
         yield i
-print('Common letters:')
-for i in common_letters('Hello!','Hi! How are you?'):
+print('-------------------Task 2-----------------------')
+words = ['Hello!','Hi! How are you?']
+print('Common letters in word "{}" with word "{}":'.format(words[0],words[1]))
+for i in common_letters(words[0],words[1]):
     print(i)
     
 print('\n')
 
-#--------------------4------------------#
+#--------------------3------------------#
 #Fibonacci algorithm
 def fibonacci(n):
     result = []
@@ -37,9 +40,9 @@ def fibonacci(n):
             new_digit = result[i-2] + result[i-3]
             result.append(new_digit)
     return result
-
-print('Fibonacci with list:\n')
-print(fibonacci(5))
+print('-------------------Task 3-----------------------')
+print('Fibonacci with list:')
+print(fibonacci(10))
 print('\n')
 
 #Fibonacci with recurency
@@ -49,10 +52,12 @@ def fib_rek(n):
     if n < 2:
         return 1
     return fib_rek(n - 1) + fib_rek(n - 2)
-print('Fibonacci with recurency:\n')
+print('Fibonacci with recurency:')
 print(fib_rek(10))
 print('\n\n')
 
+#--------------------4------------------#
+#Estimating pi value
 import random 
 def estimate_pi(n):
     num_point_circle = 0
@@ -67,5 +72,10 @@ def estimate_pi(n):
         else:
             num_point_total += 1
     return 4 * num_point_circle/num_point_total
-#print(estimate_pi(100))
+print('-------------------Task 4-----------------------')
+print('Pi value with 100 random points:')
+print(estimate_pi(100),'\n')
+
+print('Pi value with 1000000 random points:')
+print(estimate_pi(1000000))
 
